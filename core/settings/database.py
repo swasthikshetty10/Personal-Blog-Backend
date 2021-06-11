1d90cf26de5b7872c1f2df20cd19b8ca0caf1a7f
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-06-11 21:51:35 +0530

    HhHhH

--- core/settings/database.py
+++ core/settings/database.py
@@ -1,4 +1,5 @@
 from .settings import BASE_DIR , env
+import dj_database_url
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 localhost = True if env('LOCALHOST') == 'True' else False
@@ -10,13 +11,5 @@ if localhost:
         }
     }
 else :
-    DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':  env('NAME'),
-        'USER':  env('USER'),
-        'PASSWORD':  env('PASSWORD'),
-        'HOST': env('HOST'),
-        'PORT':  env('PORT'),
-    }
-    }
+    db_from_env = dj_database_url.config(conn_max_age=600)
+    DATABASES['default'].update(db_from_env)
