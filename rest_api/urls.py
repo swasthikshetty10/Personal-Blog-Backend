4cbdaee4d56191386021619dba272221f8c0c704
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-06-18 22:01:44 +0530

    /api page

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path , include
 from .views import *
 urlpatterns = [
+    path('/' , apioverview.as_view()  , name= "Over_View"),
     path('contact/' , contact.as_view()  , name= "Contact"),
     path('projectrequest/' , projectreq.as_view()  , name= "projectrequest"),
    
--- rest_api/views/__init__.py
+++ rest_api/views/__init__.py
@@ -1,3 +1,19 @@
+
 from .blog import *
 from .contact import *
 from .projectreq import *
+from rest_framework import generics
+
+
+class apioverview(generics.GenericAPIView):
+    
+    # queryset = ContactForm.objects.all()
+    pass
+    
+    # def post(self ,request , *args , **kwargs):
+    #     print(request.data)
+    #     return self.create(request, *args, **kwargs)
+    # def get_serializer(self, instance=None, data=None, many=False, partial=False):
+    #     print(data)
+    
+    
