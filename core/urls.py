dd8302d9577c83009f31e4de74461cb71ee80c02
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-06-23 22:53:25 +0530

    JWT setup

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path , include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('main_app.urls')),
-    path('api/' , include('rest_api.urls'))
+    path('api/' , include('rest_api.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- rest_api/views/__init__.py
+++ rest_api/views/__init__.py
@@ -2,9 +2,11 @@
 from .blog import *
 from .contact import *
 from .projectreq import *
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view ,  permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def apioverview(request):
     
     return Response({
