c6a5a1f1467433befdde1c624557f4bc3ba0346b
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-06-27 20:59:48 +0530

    Custom USer

--- core/settings/settings.py
+++ core/settings/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'main_app',
     'rest_api',
     'corsheaders',
+    'users',
 ]
 
 MIDDLEWARE = [
--- main_app/models.py
+++ main_app/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.db.models.fields import TextField
 from django.utils import timezone
 from django.contrib.auth.models import User
 from main_app.Utility.formatChecker import ContentTypeRestrictedFileField
-from django.template.defaultfilters import slugify
+
 class ContactForm(models.Model):
     name = models.CharField(max_length=300 , default= None)
     email = models.EmailField()
@@ -14,10 +13,6 @@ class ContactForm(models.Model):
         return self.name
 
 
-def get_filename(instance, filename):
-    title = instance.post.title
-    slug = slugify(title)
-    return "files/%s-%s" % (slug, filename)  
 class ProjectRequests(models.Model):
     user = models.ForeignKey(User , on_delete = models.CASCADE , null = True)
     projectName = models.TextField()
@@ -26,4 +21,4 @@ class ProjectRequests(models.Model):
     amountType = models.IntegerField(default=0)
     projectBudget = models.FloatField()
     finalDate = models.DateTimeField()
-    files = ContentTypeRestrictedFileField( content_types=['video/x-msvideo', 'application/pdf', 'video/mp4', 'audio/mpeg','image/jpeg' ] ,max_upload_size=20971520 , blank=True, null=True)
+    files = ContentTypeRestrictedFileField(upload_to = "files/", content_types=['video/x-msvideo', 'application/pdf', 'video/mp4', 'audio/mpeg','image/jpeg' ] ,max_upload_size=20971520 , blank=True, null=True)
