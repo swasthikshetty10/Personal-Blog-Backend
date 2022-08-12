01d595cece1f96bf1e2d536f99e1d758eaef3953
Swasthik Shetty <62538932+swasthikshetty10@users.noreply.github.com>
2022-08-12 13:26:39 +0530

    Update settings.py

--- core/settings/settings.py
+++ core/settings/settings.py
@@ -85,7 +85,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://swasthikshetty.me",
     "http://localhost:3000",
     "http://localhost:8080",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "https://swasthikshetty.vercel.app"
 ]
 CORS_ALLOW_METHODS = [
     'DELETE',
